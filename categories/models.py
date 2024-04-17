4348c6b156c68eb83472dde3ed6fbe23202df19a
Shepetkov Timerlan <sergeevp284@gmail.com>
2024-04-18 00:13:21 +0300

    инициализация миграций

--- categories/models.py
+++ categories/models.py
@@ -1,32 +1,33 @@
 from django.db import models
 
-
-#Create your models here.
+# Create your models here.
 NULLABLE = {'blank': True, 'null': True}
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100)  # Наименование
-    description = models.TextField(max_length=150)  # Описание
-    image = models.ImageField(upload_to='image_product/', name='изображение_продукта', **NULLABLE)  # Изображение
-    category = models.CharField(max_length=50)  # Категория
-    price_per_purchase = models.IntegerField(max_length=100)  # Цена за покупку
+    description = models.TextField()  # Описание
+    image = models.ImageField(upload_to='image_product/', name='image_product', **NULLABLE)  # Изображение
+
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)  # Категория
+    price_per_purchase = models.IntegerField()  # Цена за покупку
     date_of_creation = models.DateTimeField(auto_now_add=True)  # Дата создания (записи в БД)
     date_of_last_change = models.DateTimeField(auto_now=True)  # Дата последнего изменения (записи в БД)
 
     def __str__(self):
-        pass
+        return self.name
 
     class Meta:
         verbose_name = 'товар'
         verbose_name_plural = 'товары'
 
 
-
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+    description = models.TextField()
 
     def __str__(self):
-        pass
+        return self.name
 
     class Meta:
         verbose_name = 'категория'
--- online_store/settings.py
+++ online_store/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-3bx-*e0jrpum_l!4*k6^ky&s=ov9j0$c0brsbd!ji*c6k)pd#6
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,20 +68,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'online_store.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django_online_store',
         'USER': 'postgres',
         'PASSWORD': '12345',
-        'HOST': 'localhost'
+        'PORT': 5433
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -103,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -115,7 +110,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
@@ -125,3 +119,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/ 'media'
