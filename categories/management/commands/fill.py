c6d148639899f05de09bf02170038e20dafcff5e
Shepetkov Timerlan <sergeevp284@gmail.com>
2024-04-26 18:46:46 +0300

    full work

--- categories/management/commands/fill.py
+++ categories/management/commands/fill.py
@@ -6,17 +6,11 @@ import datetime
 
 class Command(BaseCommand):
 
-    @staticmethod
-    def json_read_categories():
-        with open('categories/fixtures/category.json', 'r', encoding='utf-8') as file:
-            c = json.load(file)
-            return c
-
     # Здесь мы получаем данные из фикстурв с категориями
 
     @staticmethod
     def json_read_products():
-        with open('categories/fixtures/product.json', 'r', encoding='utf-8') as file:
+        with open("categories/fixtures/product.json", "r", encoding="utf-8") as file:
             p = json.load(file)
             return p
 
@@ -28,34 +22,41 @@ class Command(BaseCommand):
         # Удалите все категории
         Category.objects.all().delete()
         # Создайте списки для хранения объектов
-        product_for_create = []
-        category_for_create = []
-
-        # Обходим все значения категорий из фиктсуры для получения информации об одном объекте
-        for category in Command.json_read_categories():
-            category_for_create.append(
-                Category(name=category['fields']['name'], description=category['fields']['description'])
+        all_object = Command.json_read_products()
+        product_for_create = [
+            Product(
+                pk=product["pk"],
+                name=product["fields"]["name"],
+                description=product["fields"]["description"],
+                category_id=product["fields"]["category"],
+                price_per_purchase=product["fields"]["price_per_purchase"],
+                date_of_creation=datetime.datetime.strptime(
+                    product["fields"]["date_of_creation"], "%Y-%m-%dT%H:%M:%S.%fZ"
+                ),
+                date_of_last_change=datetime.datetime.strptime(
+                    product["fields"]["date_of_last_change"], "%Y-%m-%dT%H:%M:%S.%fZ"
+                ),
+                manufactured_at=datetime.datetime.strptime(
+                    product["fields"]["manufactured_at"], "%Y-%m-%d"
+                ),
             )
+            for product in all_object
+            if product["model"] == "categories.product"
+        ]
+
+        category_for_create = [
+            Category(
+                pk=category["pk"],
+                name=category["fields"]["name"],
+                description=category["fields"]["description"],
+            )
+            for category in all_object
+            if category["model"] == "categories.category"
+        ]
 
         # Создаем объекты в базе с помощью метода bulk_create()
         Category.objects.bulk_create(category_for_create)
 
         # Обходим все значения продуктов из фиктсуры для получения информации об одном объекте
-        for product in Command.json_read_products():
-            category_name = product['fields']['category']
-            category, _ = Category.objects.get_or_create(name=category_name)
 
-            product_obj = Product(
-                name=product['fields']['name'],
-                description=product['fields']['description'],
-                category=category,
-                price_per_purchase=product['fields']['price_per_purchase'],
-                date_of_creation=datetime.datetime.strptime(product['fields']['date_of_creation'],
-                                                            "%Y-%m-%dT%H:%M:%S.%fZ"),
-                date_of_last_change=datetime.datetime.strptime(product['fields']['date_of_last_change'],
-                                                               "%Y-%m-%dT%H:%M:%S.%fZ"),
-                manufactured_at=datetime.datetime.strptime(product['fields']['manufactured_at'], "%Y-%m-%d")
-            )
-
-        # Создаем объекты в базе с помощью метода bulk_create()
         Product.objects.bulk_create(product_for_create)
