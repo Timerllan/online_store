6e66d1554076caeab2aa395b4e686e9d1ce36198
Shepetkov Timerlan <sergeevp284@gmail.com>
2024-05-04 02:33:00 +0300

    refactor package

--- categories/urls.py
+++ categories/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from categories.apps import CategoriesConfig  # этот иморт нужен для формирования
 
-# {%url 'имя_приложения: main_store'%} и.т.д
+# {%url 'имя_приложения: имя функции'%} и.т.д
 # оно формирует ключ чтоб переходить по значениям то есть по названию функции
 from .views import product_store, main_store
 
 app_name = CategoriesConfig.name
 
 urlpatterns = [
-    path("product_store", product_store, name="product_store"),
+    path("product_store", product_store, name="product_store"),#name - именнование функции
     path("", main_store, name="main_store"),
 ]
