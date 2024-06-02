f3a7a71769ef3297e60ef2bd1ef75c80c3d532ba
Shepetkov Timerlan <sergeevp284@gmail.com>
2024-06-03 01:00:39 +0300

    добавление store_blog в проект

--- online_store/settings.py
+++ online_store/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "categories" "blog_store",
+    "categories",
+    "blog_store",
 ]
 
 MIDDLEWARE = [
