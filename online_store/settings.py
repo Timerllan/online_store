9217824e2300f10d011fd4ae818978748aad3ff4
Shepetkov Timerlan <sergeevp284@gmail.com>
2024-06-03 00:59:24 +0300

    добавление в шаблон 2 моздание и изменение

--- online_store/settings.py
+++ online_store/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "categories",
+    "categories"
+    "blog_store",
 ]
 
 MIDDLEWARE = [
